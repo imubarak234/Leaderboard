62811984a785659c0bfc5b76d31735d09ff15c32
Mubarak <imubarak2424@gmail.com>
2021-12-14 20:55:27 +0100

    leaderboard_setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,49 @@
 import _ from 'lodash';
 import './style.css';
+import List from './store.js';
 
+const leader = new List();
+leader.addToEnd('Name', 100);
+leader.addToEnd('Name', 20);
+leader.addToEnd('Name', 50);
+leader.addToEnd('Name', 78);
+leader.addToEnd('Name', 125);
+leader.addToEnd('Name', 77);
+leader.addToEnd('Name', 42);
 
-function component() {
-  const element = document.createElement('div');
+const hold = document.querySelector('.table');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+let count = 0;
+const addToPage = (name, score) => {
+  const maindiv = document.createElement('div');
+  const message = document.createElement('p');
 
-  return element;
+  hold.append(maindiv);
+  maindiv.append(message);
+
+  maindiv.setAttribute('class', 'fields');
+  message.innerHTML = `${name}: ${score}`;
+  if(count % 2 == 0)
+  maindiv.style.backgroundColor = 'grey';
+  count += 1;
 }
 
-document.body.appendChild(component());
+leader.Lists.forEach((element) => {
+  addToPage(element.Name, element.Score);
+});
+
+const form = document.getElementById('forms');
+const add = document.getElementById('adding');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  //console.log(event.target);
+});
+
+add.addEventListener('click', (e) => {
+  const name = document.getElementById('name');
+  const score = document.getElementById('score');
+  if((name.value != '') && (score.value != ''))
+  addToPage(name.value, score.value);
+})
+
---
+++ src/store.js
@@ -0,0 +1,23 @@
+class Entry {
+  constructor(name, score){
+    this.Name = name;
+    this.Score = score;
+  }
+}
+
+class List {
+  constructor(){
+    this.Lists = [];
+  }
+
+  addToEnd(name, score){
+    const newEntry = new Entry(name, score);
+    this.Lists.push(newEntry);
+  }
+
+  removeAt(index){
+    this.Lists.splice(index, 1);
+  }
+}
+
+export {List as default};
