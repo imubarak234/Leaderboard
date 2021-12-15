adb44cd8d071170c11e9c4c50d2c2365310cc733
Mubarak <imubarak2424@gmail.com>
2021-12-15 17:50:42 +0100

    api function

---
+++ src/api.js
@@ -0,0 +1,12 @@
+async function postData(url = '', data = {}) {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
+
+export { postData as default };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
+/* eslint-disable no-console */
+
 import './style.css';
 import List from './store.js';
+import postData from './api.js';
+
+let id = '';
+let url = '';
+postData('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', { name: 'Awesome Games' })
+  .then((data) => {
+    const mike = data.result.split(' ');
+    for (let x = 0; x < mike.length; x += 1) {
+      if (mike[x] === 'ID:') { id = mike[x + 1]; }
+    }
+    url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`;
+  })
+  .catch((error) => {
+    console.error('Error: ', error);
+  });
 
 const leader = new List();
-leader.addToEnd('Name', 100);
-leader.addToEnd('Name', 20);
-leader.addToEnd('Name', 50);
-leader.addToEnd('Name', 78);
-leader.addToEnd('Name', 125);
-leader.addToEnd('Name', 77);
-leader.addToEnd('Name', 42);
 
 const hold = document.querySelector('.table');
 
@@ -22,13 +32,10 @@ const addToPage = (name, score) => {
 
   maindiv.setAttribute('class', 'fields');
   message.innerHTML = `${name}: ${score}`;
+  leader.addToEnd(name, score);
   if (count % 2 === 0) { maindiv.style.backgroundColor = 'grey'; }
   count += 1;
 };
-
-leader.Lists.forEach((element) => {
-  addToPage(element.Name, element.Score);
-});
 
 const form = document.getElementById('forms');
 const add = document.getElementById('adding');
@@ -40,5 +47,33 @@ form.addEventListener('submit', (event) => {
 add.addEventListener('click', () => {
   const name = document.getElementById('name');
   const score = document.getElementById('score');
-  if ((name.value !== '') && (score.value !== '')) { addToPage(name.value, score.value); }
+  if ((name.value !== '') && (score.value !== '')) {
+    addToPage(name.value, score.value);
+    const entry = {
+      user: name.value,
+      score: score.value,
+    };
+    postData(url, entry)
+      .then()
+      .catch((error) => {
+        console.error('Error: ', error);
+      });
+  }
+});
+
+const refresh = document.getElementById('refreshButton');
+
+refresh.addEventListener('click', () => {
+  const requestURL = url;
+  const request = new XMLHttpRequest();
+  request.open('GET', requestURL);
+  request.responseType = 'json';
+  request.send();
+
+  request.onload = () => {
+    const nextup = request.response;
+    nextup.result.forEach((element) => {
+      addToPage(element.user, element.score);
+    });
+  };
 });
